64635e20c7f9016d9327d026d4d341b96e85171b
ADS 2024.1N <36129382024.1N@senacrjedu.local>
2025-03-11 20:20:26 -0300

    11.03.2025

---
+++ infra/conexao.js
@@ -0,0 +1,12 @@
+// Código necessário para a comunicação com o BD
+import mysql from 'mysql'
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'dbsenac'
+})
+
+export default conexao
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import app from './src/app.js'
+import conexao from './infra/conexao.js'
 
 const port = 3000
 
-//Escutando a porta 3000
-app.listen(port,() =>{
-    console.log(`Servidor rodando no endereço http://localhost:${port}`)
+// Estabelecer a conexão
+conexao.connect((error) => {
+    if (error){
+        console.log(error)
+    } else {
+        console.log("Conexão realizada com sucesso!")
+        //Escutando a porta 3000
+        app.listen(port,() =>{
+        console.log(`Servidor rodando no endereço http://localhost:${port}`)
 })
 
-
+    }
+    
+})
