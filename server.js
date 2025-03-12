36c104788eeef90f36ee4c6a8ea821c067cd0d05
ADS 2024.1N <36129382024.1N@senacrjedu.local>
2025-03-11 21:45:05 -0300

    11.03.2025

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ const port = 3000
 // })
 
 //Escutando a porta 3000
-        app.listen(port,() =>{
-        console.log(`Servidor rodando no endereço http://localhost:${port}`)
+app.listen(port,() =>{
+console.log(`Servidor rodando no endereço http://localhost:${port}`)
 })
