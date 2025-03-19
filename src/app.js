49d1fa6a8f4bdb171c41778856d27a9b1a50fd57
ADS 2024.2N <36129382024.2N@senacrjedu.local>
2025-03-18 21:33:58 -0300

    18.03.2025

--- src/app.js
+++ src/app.js
@@ -25,6 +25,8 @@ app.use(express.json())
 
 //Outras rotas
 
+// **ANOTAR IDEIAS PARA MELHORAR O CÓDIGO, ENTRADA, SAÍDA, ETC**
+
 //listar
 app.get('/listas',(req,res)=>{
     const sql = "SELECT * FROM dbsenac.alunos;"
@@ -78,11 +80,47 @@ conexao.query(sql, aluno, (error,result)=>{
 })
 
 
-//deletar errado 
-app.delete('/listas',(req,res)=>{
-    listas.pop(req.body)
-    res.status(201).send('Aluno apagado com sucesso!')
+// //deletar errado 
+// app.delete('/listas',(req,res)=>{
+//     listas.pop(req.body)
+//     res.status(201).send('Aluno apagado com sucesso!')
+// })
+
+
+//delete
+app.delete('/listas/:id',(req,res)=>{
+  //let index  = req.params.id
+  //console.log(index)
+  //res.json(buscarAlunoporId(req.params.id));
+  const id = req.params.id
+  const sql = "DELETE FROM dbsenac.alunos WHERE id=?;"
+  conexao.query(sql, id, (error,result)=>{
+    if (error) {
+      console.log(error)
+      res.status(404).json({'error':error})
+    } else {
+        res.status(200).json(result)
+    }
+  })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Rota de exemplo teste: Retorna uma mensagem simples
