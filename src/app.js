99e07f909a69a2c1bf9fb6ccea23eac4fc3610a2
ADS 2024.2N <36129382024.2N@senacrjedu.local>
2025-03-25 20:58:09 -0300

    25.03.2025

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import conexao from './app/database/conexao.js'
+import AlunoController from './app/controllers/AlunoController.js'
+
 const app = express()
 
 //indcar para o express para usar o body com jason
@@ -11,110 +12,23 @@ app.use(express.json())
 // **ANOTAR IDEIAS PARA MELHORAR O CÓDIGO, ENTRADA, SAÍDA, ETC**
 
 //listar
-app.get('/listas', (req, res) => {
-  const sql = "SELECT * FROM dbsenac.alunos;"
-  conexao.query(sql, (error, result) => {
-    if (error) {
-      console.log(error)
-      res.status(404).json({ 'error': error })
-    } else {
-      res.status(200).send(result);
-    }
-  })
-})
+app.get('/listas', AlunoController.index)
 
 
 //bucar por id
-app.get('/listas/:id', (req, res) => {
-  const id = req.params.id
-  const sql = "SELECT * FROM dbsenac.alunos WHERE id=?;"
-  conexao.query(sql, id, (error, result) => {
-    const row = result[0]
-    if (error) {
-      console.log(error)
-      res.status(404).json({ 'error': error })
-    } else {
-      if (row) {
-        res.status(200).send(row);
-      } else {
-        res.status(404).send("Aluno não encontrado!")
-      }
-    }
-  })
-})
+app.get('/listas/:id', AlunoController.show)
 
 
 //criar
-app.post('/listas', (req, res) => {
-  const aluno = req.body
-  const sql = "INSERT INTO `dbsenac`.`alunos` SET ?;"
-  conexao.query(sql, aluno, (error, result) => {
-    if (error) {
-      console.log(error)
-      res.status(404).json({ 'error': error })
-    } else {
-      res.status(201).send(aluno);
-    }
-  })
-})
-
-
-// deletar por id
-app.delete('/delete/:id', (req, res) => {
-  const id = req.params.id
-  const sql = "DELETE FROM dbsenac.alunos WHERE id=?;"
-  conexao.query(sql, [id], (error, result) => {
-    const row = result[0]
-    if (error) {
-      console.log(error)
-      return res.status(500).json({ 'error': 'Erro interno do servidor', 'details': error })
-    }
-
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ 'message': `Aluno com ID ${id} não encontrado.` })
-    }
-
-    res.status(200).json({ 'message': `Aluno com ID ${id} excluído com sucesso.` })
-
-  })
-})
+app.post('/listas', AlunoController.store)
 
 
 // update por id
-app.put('/update/:id', (req, res) => {
-  const id = req.params.id;
-  const aluno = req.body; // Pega as informações do aluno do corpo da requisição
+app.put('/update/:id', AlunoController.update)
 
-  const checkSql = "SELECT * FROM dbsenac.alunos WHERE id=?;";
-  conexao.query(checkSql, [id], (error, result) => {
 
-    if (error) {
-      console.log(error);
-      return res.status(500).json({ 'error': 'Erro interno do servidor', 'details': error });
-    }
-
-    if (result.length === 0) { // Corrigido o erro de digitação aqui
-      return res.status(404).json({ 'message': `Aluno com ID ${id} não encontrado.` });
-    }
-
-    // Se o aluno existir, faça o update
-    const sql = "UPDATE dbsenac.alunos SET ? WHERE id=?;";
-    conexao.query(sql, [aluno, id], (error, updateResult) => {
-      if (error) {
-        console.log(error);
-        return res.status(400).json({ 'error': 'Erro ao atualizar o aluno', 'details': error });
-      }
-
-      // Verifica se a atualização foi bem sucedida
-      if (updateResult.affectedRows === 0) {
-        return res.status(404).json({ 'message': `Aluno com ID ${id} não encontrado para atualização.` });
-      }
-
-      // Retorna o aluno atualizado
-      res.status(200).json({ 'message': `Aluno com ID ${id} atualizado com sucesso.` });
-    });
-  });
-});
+// deletar por id
+app.delete('/delete/:id', AlunoController.delete)
 
 
 
--- src/app/controllers/AlunoController.js
+++ src/app/controllers/AlunoController.js
@@ -1,18 +1,107 @@
+import conexao from "../database/conexao.js";
+
 class AlunoController {
     // listar tudo
-    index(){}
+    index(req, res) {
+        const sql = "SELECT * FROM dbsenac.alunos;"
+        conexao.query(sql, (error, result) => {
+            if (error) {
+                // console.log(error)
+                res.status(404).json({ 'error': error })
+            } else {
+                res.status(200).json(result);
+            }
+        })
+    }
 
     // listar por id
-    show(){}
+    show(req, res) {
+        const id = req.params.id
+        const sql = "SELECT * FROM dbsenac.alunos WHERE id=?;"
+        conexao.query(sql, id, (error, result) => {
+            const row = result[0]
+            if (error) {
+                console.log(error)
+                res.status(404).json({ 'error': error })
+            } else {
+                if (row) {
+                    res.status(200).send(row);
+                } else {
+                    res.status(404).send("Aluno não encontrado!")
+                }
+            }
+        })
+    }
 
     //criar dados
-    store(){}
+    store(req, res) {
+        const aluno = req.body
+        const sql = "INSERT INTO `dbsenac`.`alunos` SET ?;"
+        conexao.query(sql, aluno, (error, result) => {
+            if (error) {
+                console.log(error)
+                res.status(404).json({ 'error': error })
+            } else {
+                res.status(201).send(aluno);
+            }
+        })
+    }
 
     // atualizar dados
-    update(){}
+    update(req, res) {
+        const id = req.params.id;
+        const aluno = req.body; // Pega as informações do aluno do corpo da requisição
+
+        const checkSql = "SELECT * FROM dbsenac.alunos WHERE id=?;";
+        conexao.query(checkSql, [id], (error, result) => {
+
+            if (error) {
+                console.log(error);
+                return res.status(500).json({ 'error': 'Erro interno do servidor', 'details': error });
+            }
+
+            if (result.length === 0) { // Corrigido o erro de digitação aqui
+                return res.status(404).json({ 'message': `Aluno com ID ${id} não encontrado.` });
+            }
+
+            // Se o aluno existir, faça o update
+            const sql = "UPDATE dbsenac.alunos SET ? WHERE id=?;";
+            conexao.query(sql, [aluno, id], (error, updateResult) => {
+                if (error) {
+                    console.log(error);
+                    return res.status(400).json({ 'error': 'Erro ao atualizar o aluno', 'details': error });
+                }
+
+                // Verifica se a atualização foi bem sucedida
+                if (updateResult.affectedRows === 0) {
+                    return res.status(404).json({ 'message': `Aluno com ID ${id} não encontrado para atualização.` });
+                }
+
+                // Retorna o aluno atualizado
+                res.status(200).json({ 'message': `Aluno com ID ${id} atualizado com sucesso.` });
+            });
+        });
+    }
 
     // remover dados
-    delete(){}
+    delete(req, res) {
+        const id = req.params.id
+        const sql = "DELETE FROM dbsenac.alunos WHERE id=?;"
+        conexao.query(sql, [id], (error, result) => {
+            const row = result[0]
+            if (error) {
+                console.log(error)
+                return res.status(500).json({ 'error': 'Erro interno do servidor', 'details': error })
+            }
+
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ 'message': `Aluno com ID ${id} não encontrado.` })
+            }
+
+            res.status(200).json({ 'message': `Aluno com ID ${id} excluído com sucesso.` })
+
+        })
+    }
 
 }
 
