b0407d2a08724e93fbdf422a57b928305e6133d2
ADS 2024.2N <36129382024.2N@senacrjedu.local>
2025-03-25 20:39:35 -0300

    25.03.2025

---
+++ src/app/controllers/AlunoController.js
@@ -0,0 +1,20 @@
+class AlunoController {
+    // listar tudo
+    index(){}
+
+    // listar por id
+    show(){}
+
+    //criar dados
+    store(){}
+
+    // atualizar dados
+    update(){}
+
+    // remover dados
+    delete(){}
+
+}
+
+// padrão singleton
+export default new AlunoController()
