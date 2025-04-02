833068bb2ac97b68637ac68237bf1245f4c23a80
ADS 2024.2N <36129382024.2N@senacrjedu.local>
2025-04-01 21:21:24 -0300

    01.04.2025

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -11,4 +11,23 @@ const conexao = mysql.createConnection({
 
 conexao.connect()
 
+/**
+ * Executa o código sql com ou sem valores
+ * @param {string} sql instrução sql a ser executada
+ * @param {string=id | [aluno, id]} params a serem passados para o sql
+ * @param {string} mesageReject mensagem do Promise reject a ser exibida
+ * @returns objeto da Promise
+ */
+
+export const consulta = (sql, params, messageReject) => {
+    return new Promise((resolve, reject) => {
+        conexao.query(sql, params, (error, result) => {
+            if (error) return reject(messageReject)
+            // mostra o resultado de forma adequada (transforma para string)
+            const row = JSON.parse(JSON.stringify(result))
+            return resolve(row)
+        })
+    })
+}
+
 export default conexao
